91c316c391e6f386011e8918794ad895af8f7c96
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-08-31 15:46:51 +0300

    pointer control implement

--- src/com/mohamed/receiverKey/PointerAction.java
+++ src/com/mohamed/receiverKey/PointerAction.java
@@ -9,6 +9,55 @@ package com.mohamed.receiverKey;
  *
  * @author mohamednagy
  */
-public enum PointerAction {
+public class PointerAction{
     
+    static enum PointerMoving {
+        INCREASE_X_POSITION(0x001),
+        INCREASE_Y_POSITION(0x002),
+        DECREASE_X_POSITION(0x004),
+        DECREASE_Y_POSITION(0x005),
+        INCREASE_X_Y_POSITION(0x003),
+        DECREASE_X_Y_POSITION(0x006),
+        INCREASE_X_DECREASE_Y_POSITION(0x007),
+        INCREASE_Y_DECREASE_X_POSITION(0x008);
+
+        public final int mCode;
+
+        private PointerMoving(final int code) {
+            mCode = code;
+        }
+      
+    }
+    
+    static enum PointerSPEED {
+        SPEED_LEVEL_ONE(0x001),
+        SPEED_LEVEL_TWO(0x002),
+        SPEED_LEVEL_THREE(0x004),
+        SPEED_LEVEL_FOUR(0x005),
+        SPEED_LEVEL_FIVE(0x003),
+        SPEED_LEVEL_SIX(0x006),
+        SPEED_LEVEL_SEVEN(0x007),
+        SPEED_LEVEL_EIGHT(0x008);
+
+        public final int mCode;
+
+        private PointerSPEED(final int code) {
+            mCode = code;
+        }
+      
+    }
+    
+    
+    static enum PointerClick{
+        SINGLE_LEFT_CLICK(0x009),
+        DOUBLE_LEFT_CLICK(0x00A),
+        RIGHT_CLICK(0x00B),
+        RIGHT_CLICK_WITH_MOVING(0x00C);
+        
+        public final int mCode;
+        
+        private PointerClick(int code){
+            mCode = code;
+        }
+    }
 }
--- src/com/mohamed/receiverKey/PointerControl.java
+++ src/com/mohamed/receiverKey/PointerControl.java
@@ -5,10 +5,60 @@
  */
 package com.mohamed.receiverKey;
 
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Robot;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mohamednagy
  */
 public class PointerControl {
     
+    public static final int DEC_X_COOR = PointerAction.PointerMoving.DECREASE_X_POSITION.mCode;
+    public static final int DEC_Y_COOR = PointerAction.PointerMoving.DECREASE_Y_POSITION.mCode;
+    public static final int INC_X_COOR = PointerAction.PointerMoving.INCREASE_X_POSITION.mCode;
+    public static final int INC_Y_COOR = PointerAction.PointerMoving.INCREASE_Y_POSITION.mCode;
+    public static final int DEC_X_Y_COOR = PointerAction.PointerMoving.DECREASE_X_Y_POSITION.mCode;
+    public static final int INC_X_Y_COOR = PointerAction.PointerMoving.INCREASE_X_Y_POSITION.mCode;
+    public static final int DEC_X_INC_Y_COOR = PointerAction.PointerMoving.INCREASE_Y_DECREASE_X_POSITION.mCode;
+    public static final int DEC_Y_INC_X_COOR = PointerAction.PointerMoving.INCREASE_X_DECREASE_Y_POSITION.mCode;
+    
+    
+    
+    public static void setPointerMovingAction(
+            PointerAction.PointerMoving pointerMoving, 
+            final int MOVING_SPEED){
+        final int POINTER_LOCATION_X_COORDINATE = MouseInfo.getPointerInfo().getLocation().x;
+        final int POINTER_LOCATION_Y_COORDINATE = MouseInfo.getPointerInfo().getLocation().y;
+        
+        if     (pointerMoving.mCode == DEC_X_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE - MOVING_SPEED, POINTER_LOCATION_Y_COORDINATE);
+        else if(pointerMoving.mCode == INC_X_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE + MOVING_SPEED, POINTER_LOCATION_Y_COORDINATE);
+        else if(pointerMoving.mCode == DEC_Y_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE, POINTER_LOCATION_Y_COORDINATE - MOVING_SPEED);
+        else if(pointerMoving.mCode == INC_Y_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE, POINTER_LOCATION_Y_COORDINATE + MOVING_SPEED);
+        else if(pointerMoving.mCode == INC_X_Y_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE + MOVING_SPEED, POINTER_LOCATION_Y_COORDINATE + MOVING_SPEED);
+        else if(pointerMoving.mCode == DEC_X_Y_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE - MOVING_SPEED, POINTER_LOCATION_Y_COORDINATE - MOVING_SPEED);
+        else if(pointerMoving.mCode == DEC_X_INC_Y_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE - MOVING_SPEED, POINTER_LOCATION_Y_COORDINATE + MOVING_SPEED);
+        else if(pointerMoving.mCode == DEC_Y_INC_X_COOR)
+            setPointerLocation(POINTER_LOCATION_X_COORDINATE + MOVING_SPEED, POINTER_LOCATION_Y_COORDINATE - MOVING_SPEED);
+        
+          
+    }
+   
+    private static void setPointerLocation(final int X_COORDINATE, final int Y_COORDINATE){
+        try {
+            new Robot().mouseMove(X_COORDINATE, Y_COORDINATE);
+        } catch (AWTException ex) {
+            Logger.getLogger(PointerControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
